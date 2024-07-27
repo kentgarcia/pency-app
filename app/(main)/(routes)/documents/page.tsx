da51e6e313825f8f5ced2953476999c966e96ffe
kentgarcia <kentdgarcia@iskolarngbayan.pup.edu.ph>
2024-07-27 20:19:44 +0800

    feat: Add sidebar page component and update dependencies

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+  return <div>This is the documents page.</div>;
+};
+
+export default DocumentsPage;
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useConvexAuth } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
@@ -12,45 +12,41 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export const Navbar = () => {
-    const { isAuthenticated, isLoading} = useConvexAuth();
-    const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
 
-    return (
-        <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
-            <Logo/>
-            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                {isLoading && (
-                    <Spinner/>
-                )}
-                {isAuthenticated && !isLoading && (
-                    <>
-                    <SignInButton mode="modal">
-                        <Button variant="ghost" size="sm">
-                            Log in
-                        </Button>
-                    </SignInButton>
-                    <SignInButton mode="modal">
-                        <Button size="sm">
-                            Get Pency free
-                        </Button>
-                    </SignInButton>
-                    </>
-                )}
-                {
-                    isAuthenticated && !isLoading && (
-                        <>
-                        <Button variant="ghost" size="sm" asChild>
-                            <Link href="/documents">
-                            Enter Pency
-                            </Link>
-                        </Button>
-                        <UserButton
-                        afterSignOutUrl="/"/>
-                        </>
-                    )
-                }
-                <ModeToggle/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Pency free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Pency</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
